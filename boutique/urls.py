21225c4b180b311e86d5a74b5a89e9ef4f8bf175
Massamba Sene <smmassamba@ept.sn>
2022-06-02 14:56:21 +0000

    Modification de l'UI

--- boutique/urls.py
+++ boutique/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('achat/<slug:slug>', views.book_buy, name='book_buy'),
     path('boutique/<slug:category_slug>/',
          views.book_category, name='book_category'),
-    # path('add/revue/<slug:slug>', views.add_revue, name='revue'),
     path('modifier/<slug:slug>', views.mod_revue, name='modifier'),
     path('delete/', views.del_revue, name='supprimer'),
 ]
